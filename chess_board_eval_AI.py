5bbbacf71c4d2a7845a694da00b00a09f7448f45
Marcus1506 <116986601+Marcus1506@users.noreply.github.com>
2023-02-18 01:57:48 +0100

    generated the dataset and preparing for training of first model

--- chess_board_eval_AI.py
+++ chess_board_eval_AI.py
@@ -10,9 +10,10 @@ of chess which ended either in a draw, checkmate or stalemate.
 One chess game lasts 40 moves on average (80 positions), so around 4*10^6 board states build
 our dataset.
 
-Each boardstate of every game has one feature in this case, the evaluation of the
-board state, which is centipawns (cp), where 100 cp = 1 pawn which is an arbritrary unit and
-is > 0 if white has the advantage and < 0 if black is winning.
+Each boardstate of every game is endcoded according to some representation.
+Therefore the position of each piece on the board and their estimated initial values are our features.
+The evaluation of the board state, which is in centipawns (cp), where 100 cp = 1 pawn, which is an arbritrary
+unit and is > 0 if white has the advantage and < 0 if black is winning.
 
 -----------------------------------------------------------------------------------------
 
@@ -29,6 +30,7 @@ stockfish.csv, such that I have only boardstates with their corresponding evalua
 import re
 from chess_module import *
 import json
+import random
 
 def extract_moves(source_file, target_file):
     matches=[]
@@ -43,7 +45,7 @@ def extract_moves(source_file, target_file):
                 if line.startswith("1. "): # detect new match
                     matches.append([])
                 cleaned_line=re.sub(remove_patterns, "", line) # removing what matches patterns
-                moves_string=cleaned_line.split()
+                moves_string=cleaned_line.split() # splits strings with whitespace delimiters
                 matches[-1].extend(moves_string)
     
     json_data=json.dumps(matches)
@@ -51,7 +53,7 @@ def extract_moves(source_file, target_file):
     with open(target_file, "w") as target:
         target.write(json_data)
     
-def generate_board_states_board_rep(source_file, target_dir):
+def generate_board_states_board_rep(source_file, target_dir): # uses lots of RAM, we could divide it beforehand
     boards=[]
     gamestate=chess_board()
     count=0
@@ -75,13 +77,34 @@ def generate_board_states_board_rep(source_file, target_dir):
 
 def combine_evals_with_board_rep(eval_file, boards_rep_file, target_file):
     boards=np.load(boards_rep_file)
+    boards=np.array([board.flatten() for board in boards]) # flatten the arrays for neural network input
+    boards=boards.tolist()
+    eval_scores=[]
+    
+    remove_pattern=re.compile(r"^\d+,")
     
     with open(eval_file) as evals:
         for line in evals:
             if re.match(r"^\d", line):
-                pass
+                cleaned_line=re.sub(remove_pattern, "", line)
+                eval_scores_for_match=cleaned_line.split()
+                if not eval_scores_for_match:
+                    eval_scores.append("") # keep dimensions equal
+                eval_scores.extend(eval_scores_for_match)
             else:
                 continue
+    
+    labeled_data=[]
+    
+    for i in range(len(eval_scores)-1, -1, -1):
+        if eval_scores[i]!='NA' and eval_scores[i]!='': # remove NA (no stockfish evaluation) and instant resigns (no moves played)
+            labeled_data.append([boards[i], int(eval_scores[i])])
+        else:
+            print(i)
+    
+    with open(target_file+".json", 'w') as source:
+        json.dump(labeled_data, source)
+    
     
 '''
 Extract the moves from the data and write them into a JSON file:
@@ -99,7 +122,23 @@ Generate board states from the extracted moves, and write them ordered into a nu
 Now get board evaluations from stockfish.csv and pair them with corresponding board states to form our base dataset for the NN:
 '''
 
-combine_evals_with_board_rep("data/evaluated_positions/extracted_moves.json", "data/evaluated_positions/boards_board_representation.npy", "test")
+# combine_evals_with_board_rep("data/evaluated_positions/stockfish.csv", "data/evaluated_positions/boards_board_representation.npy", "data/evaluated_positions/labeled_data_board_rep")
+
+
+
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense
+from sklearn.metrics import accuracy_score
+
+# initialize model:
+model=Sequential()
+model.add(Dense(units=64, input_dim=64)) # here also activations?
+model.add(Dense(units=80, activation='relu'))
+model.add(Dense(units=80, activation='relu'))
+model.add(Dense(units=80, activation='relu'))
+model.add(Dense(units=1)) # what activations makes sense here?
+
+
 
 
 
